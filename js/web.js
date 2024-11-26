4b715c87bdf77b5a6505c91771aa5b67d6256185
Prasad-jayathunga <prasad.jayathunga1992@gmail.com>
2024-11-26 06:29:57 -0800

    BMI Calculator

--- js/web.js
+++ js/web.js
@@ -4,10 +4,10 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "15px 10px";
+    document.getElementById("navbar").style.padding = "10px 5px";
     document.getElementById("logo").style.fontSize = "20px";
   } else {
-    document.getElementById("navbar").style.padding = "50px 10px";
+    document.getElementById("navbar").style.padding = "15px 10px";
     document.getElementById("logo").style.fontSize = "20px";
   }
 }
@@ -39,3 +39,47 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+/*-----------------------------start eyes--------------------------*/
+
+/*--------------------------------Start BMI Calculator----------------------------------------*/
+
+const btn = document.getElementById("calculate");
+
+btn.addEventListener("click", function () {
+  let height = document.querySelector("#height").value;
+  let weight = document.querySelector("#weight").value;
+
+  if (height == "" || weight == "") {
+    alert("Please fill out the input fields!");
+    return;
+  }
+
+  // BMI = weight in KG / (height in m * height in m)
+
+  height = height / 100;
+
+  let BMI = weight / (height * height);
+
+  BMI = BMI.toFixed(2);
+
+  document.querySelector("#result").innerHTML = BMI;
+
+  let status = "";
+
+  if (BMI < 18.5) {
+    status = "Underweight";
+  }
+  if (BMI >= 18.5 && BMI < 25) {
+    status = "Healthy";
+  }
+  if (BMI >= 25 && BMI < 30) {
+    status = "Overweight";
+  }
+  if (BMI >= 30) {
+    status = "Obese";
+  }
+  document.querySelector(
+    ".comment"
+  ).innerHTML = `Comment: you are <span id="comment">${status}</span>`;
+});
+/*----------------------------smooth scroll--------------------------------------*/
